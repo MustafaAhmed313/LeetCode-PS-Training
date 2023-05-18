644253837b69cf0a292dca4064f42968ced9d438
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-05-18 11:39:37 +0300

    xd

--- Easy/Main.java
+++ Easy/Main.java
@@ -1,23 +1,6 @@
 import java.util.*;
-
 public class Main {
-
     public static void main(String[] args) {
 
     }
-
-    class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
 }
-
