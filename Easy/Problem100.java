a6a3ad118c99cac678bafb26731436900e59451d
MustafaAhmed313 <mostafa3132004@gmail.com>
2023-07-02 01:50:56 +0300

    adding a prob.

---
+++ Easy/Problem100.java
@@ -0,0 +1,19 @@
+public class Problem100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if ((p == null && q != null) || (p != null && q == null)) return false;
+        return p.val == q.val && isSameTree(p.left , q.left) && isSameTree(p.right , q.right);
+    }
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
